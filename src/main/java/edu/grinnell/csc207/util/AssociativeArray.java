1c2e6032555eb5fa15316b747fa11760ff6d0202
mitchydev <paivamit@grinnell.edu>
2024-10-02 14:19:46 -0500

    Added hasKey and get methods

--- src/main/java/edu/grinnell/csc207/util/AssociativeArray.java
+++ src/main/java/edu/grinnell/csc207/util/AssociativeArray.java
@@ -3,9 +3,8 @@ package edu.grinnell.csc207.util;
 import static java.lang.reflect.Array.newInstance;
 
 /**
- * A basic implementation of Associative Arrays with keys of type K
- * and values of type V. Associative Arrays store key/value pairs
- * and permit you to look up values by key.
+ * A basic implementation of Associative Arrays with keys of type K and values of type V.
+ * Associative Arrays store key/value pairs and permit you to look up values by key.
  *
  * @param <K> the key type
  * @param <V> the value type
@@ -44,11 +43,10 @@ public class AssociativeArray<K, V> {
   /**
    * Create a new, empty associative array.
    */
-  @SuppressWarnings({ "unchecked" })
+  @SuppressWarnings({"unchecked"})
   public AssociativeArray() {
     // Creating new arrays is sometimes a PITN.
-    this.pairs = (KVPair<K, V>[]) newInstance((new KVPair<K, V>()).getClass(),
-        DEFAULT_CAPACITY);
+    this.pairs = (KVPair<K, V>[]) newInstance((new KVPair<K, V>()).getClass(), DEFAULT_CAPACITY);
     this.size = 0;
   } // AssociativeArray()
 
@@ -79,59 +77,67 @@ public class AssociativeArray<K, V> {
   // +----------------+
 
   /**
-   * Set the value associated with key to value. Future calls to
-   * get(key) will return value.
+   * Set the value associated with key to value. Future calls to get(key) will return value.
    *
-   * @param key
-   *   The key whose value we are seeting.
-   * @param value
-   *   The value of that key.
+   * @param key The key whose value we are seeting.
+   * @param value The value of that key.
    *
-   * @throws NullKeyException
-   *   If the client provides a null key.
+   * @throws NullKeyException If the client provides a null key.
    */
   public void set(K key, V value) throws NullKeyException {
-    if (key == null)
-  } // set(K,V)
+    if (key == null) {
+      throw new NullKeyException();
+    } // if statement
+    for (int i = 0; i < this.size; i++) {
+      if (this.pairs[i].key.equals(key))
+        this.pairs[i].val = value;
+      return;
+    } // for loop
+  }// set(K,V)
 
   /**
    * Get the value associated with key.
    *
-   * @param key
-   *   A key
+   * @param key A key
    *
-   * @return
-   *   The corresponding value
+   * @return The corresponding value
    *
-   * @throws KeyNotFoundException
-   *   when the key is null or does not appear in the associative array.
+   * @throws KeyNotFoundException when the key is null or does not appear in the associative array.
    */
   public V get(K key) throws KeyNotFoundException {
-    return null; // STUB
+    for (int i = 0; i < this.size; i++) {
+      if (this.pairs[i].key.equals(key)) {
+        return this.pairs[i].val;
+      } // if statement
+    } // for loop
+    throw new KeyNotFoundException();
   } // get(K)
 
   /**
-   * Determine if key appears in the associative array. Should
-   * return false for the null key, since it cannot appear.
+   * Determine if key appears in the associative array. Should return false for the null key, since
+   * it cannot appear.
    *
-   * @param key
-   *   The key we're looking for.
+   * @param key The key we're looking for.
    *
    * @return true if the key appears and false otherwise.
    */
   public boolean hasKey(K key) {
-    return false; // STUB
+    for (int i = 0; i < this.size; i++) {
+      if (this.pairs[i].key.equals(key)) {
+        return true;
+      } // if statement
+    } // for loop
+    return false;
   } // hasKey(K)
 
   /**
-   * Remove the key/value pair associated with a key. Future calls
-   * to get(key) will throw an exception. If the key does not appear
-   * in the associative array, does nothing.
+   * Remove the key/value pair associated with a key. Future calls to get(key) will throw an
+   * exception. If the key does not appear in the associative array, does nothing.
    *
-   * @param key
-   *   The key to remove.
+   * @param key The key to remove.
    */
   public void remove(K key) {
+
     // STUB
   } // remove(K)
 
@@ -156,20 +162,17 @@ public class AssociativeArray<K, V> {
   } // expand()
 
   /**
-   * Find the index of the first entry in `pairs` that contains key.
-   * If no such entry is found, throws an exception.
+   * Find the index of the first entry in `pairs` that contains key. If no such entry is found,
+   * throws an exception.
    *
-   * @param key
-   *   The key of the entry.
+   * @param key The key of the entry.
    *
-   * @return
-   *   The index of the key, if found.
+   * @return The index of the key, if found.
    *
-   * @throws KeyNotFoundException
-   *   If the key does not appear in the associative array.
+   * @throws KeyNotFoundException If the key does not appear in the associative array.
    */
   int find(K key) throws KeyNotFoundException {
-    throw new KeyNotFoundException();   // STUB
+    throw new KeyNotFoundException(); // STUB
   } // find(K)
 
 } // class AssociativeArray
